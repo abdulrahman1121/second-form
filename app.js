99744672a16ad2cabf79a5f2dbf9646ef816a9ea
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2024-01-13 16:39:22 -0800

    Finished with the code, but it's not working properly, need to debug a little and fix styling, also don't forget to add a success message

--- app.js
+++ app.js
@@ -19,9 +19,61 @@ form.addEventListener('submit', (e) => {
 
 const checkInputs = () => {
     //get value from inputs
-    const usernameValue = username.value
-    const emailValue = email.value
-    const passwordValue = password.value
-    const password2Value = password2.value
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(usernameValue === ''){
+        //show error
+        //add error class
+        setErrorFor(username, 'Username cant be blank');
+    }else{
+        setSuccessFor(username);
+    }
+
+    if(emailValue === ''){
+        setErrorFor(email, 'Email cant be blank');
+    }else if(isEmail(emailValue)){
+        setErrorFor(email, 'Email is not valid');
+    }else{
+        setSuccessFor(email);
+    }
+
+    if(passwordValue === ''){
+        setErrorFor(password, 'Password cant be blank');
+    }else{
+        setSuccessFor(password)
+    }
+
+    if(password2Value === ''){
+        setErrorFor(password2, 'Password2 cant be blank');
+    }else if(passwordValue !== password2Value){
+        setErrorFor(password2, 'Password2 must match password')
+    }else{
+        setSuccessFor(password2);
+    }
+
+    //show a success message
 }
 
+
+const setErrorFor = (input, message) => {
+    const formControl = input.parentElement; //.form-control
+    const small = formControl.querySelector('small');
+
+    //add error message
+    small.innerText = message;
+
+    // add error class
+    formControl.className = 'form-control error';
+}
+
+const setSuccessFor = (input) => {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success'; 
+}
+
+const isEmail = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
